5c36f3558a61b84a712f28602a945c4a6a959767
kaba1234 <kabajunior24@gmail.com>
2024-12-16 18:23:41 +0000

    Add files via upload

---
+++ .github/workflows/freeexercise.py
@@ -0,0 +1,88 @@
+# # x = 5
+# if x > 8 :
+#     print("hello world")
+
+# print("end")
+
+# x = 4
+# if x <= 4 :
+#     print("welcome")
+# print("try again")
+
+# x = 4
+# if x >= 5 :
+#     print("welcome")
+# print("try again")
+
+# x = 5
+# if x > 9 :
+#     print("hello!")
+# else :
+#         print("bye!")
+# print("end")
+
+# my_real = input("what is my_real age")
+# print(my_real)
+
+# a = 5
+# b = 10
+
+# print(a > 0)
+# print(b > 0)
+# print(a > 0 and b > 0)
+
+# age = 17 
+# if age >= 18 :
+#     print("you are an adult.")
+# else : 
+#     print("you are a minor,")
+
+# print(type(17))
+# print(type(10.3))
+# print(type("hello, world"))
+# print(type(True))
+
+# name = "kaba junior"
+# first_name = name[1:11]
+# print(first_name)
+
+# name = input("wnat is your name ? :")
+# age = int(input("how old are you ? :"))
+# height = float(input("how tall are you ? :"))
+
+# print("hello" + name)
+# print("you are " + str(age) + "years old")
+# print("you are " + str(height) + "cm tall")
+
+# freecodecamp = "freecodecamp"
+# print(freecodecamp.index("p"))
+students = [[42, 'Mofor', True, 178.23], [24, 'Kaba', True, 170.324], [32, 'Fru', True, 200.324]]
+kelly = [20, 'Kelly', False, 52.4]
+students.append(kelly)
+
+# print(kelly.sort("age"))
+# age = (students [3][0])
+# print(age)
+##ASSIGNMENT:
+#Write if/elif/else statements that use all 4 variables below
+#You must use if condition, elif condition and else conditioin
+kelly_age = 20
+kaba_age = 24
+Mofor_age = 42
+Louis_age = 32
+# Comparison operators
+# >, <, ==, >=, <=, !=
+
+#For example:
+
+if kelly_age > kaba_age:
+    print("true")
+elif kaba_age < Mofor_age:
+    print("true")
+elif Mofor_age == Louis_age:
+    print("true")
+elif Louis_age >= Mofor_age:
+    print("true")
+else:
+    print("none of the above is correct")
+    
